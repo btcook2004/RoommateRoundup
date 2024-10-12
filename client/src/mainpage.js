1fc280f7861ecdbd75f7a296d2adae22bd28588e
mbucataru <mbucatarugithub@gmail.com>
2024-10-11 21:35:31 -0400

    Add Increment1 Documents

--- client/src/mainpage.js
+++ client/src/mainpage.js
@@ -3,7 +3,7 @@ function leftButtonClicked()
     const request1 = new Request("http://localhost:3000/console", {
         method: "POST",
         headers: {
-            "Content-Type": "application/json",  // Tell the server you're sending JSON
+            "Content-Type": "application/json",
         },
         body: JSON.stringify({
             text: "Left was clicked"
