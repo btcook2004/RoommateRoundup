6deb5bf4d5ab323c750d4de19a179dd078b5b2f5
mbucataru <mbucatarugithub@gmail.com>
2024-10-28 10:46:33 -0400

    Touch up React

--- client/src/messagepage.jsx
+++ client/src/messagepage.jsx
@@ -1,6 +1,39 @@
+function NavBar()
+{
+    return (
+        <nav>
+            <ul className="navBar">
+                <li>
+                    <a href="../index.html">Swipe Page</a>
+                </li>
+                <li>
+                    <a href="./signup.html">Sign-up Page</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+function ListItem(props)
+{
+    // props: {animal: "Dog",
+    return <li>{props.animal}{props.suffix}</li>;
+}
+
 function MessagePage()
 {
-    return <div><h1>Messages</h1></div>
+    let animals = ['Dog', 'Lemur', 'Wombat'];
+    return (
+        <div>
+            <NavBar />
+            <div>
+                <h1>Messages</h1>
+            </div>
+            <ul>
+                {animals.map((animal) => <ListItem key={animal} animal={animal} suffix={"Woo"}> </ListItem>)}
+            </ul>
+        </div>
+    );
 }
 
 export default MessagePage
