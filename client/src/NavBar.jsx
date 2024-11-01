8359bea1aa8c60f793d51e034a552dd39548d835
btcook2004 <btcook.2004@gmail.com>
2024-11-01 15:47:39 -0400

    Index page reactified, broke messages

--- client/src/NavBar.jsx
+++ client/src/NavBar.jsx
@@ -1,17 +1,18 @@
+
 function NavBar()
 {
     return (
         <nav>
             <ul className="navBar">
                 <li>
-                    <a href="../index.html">Swipe Page</a>
+                    <a href="../messagepage.html">Message Page</a>
                 </li>
                 <li>
                     <a href="./signup.html">Sign-up Page</a>
                 </li>
             </ul>
         </nav>
-    )
+    );
 }
 
-export default NavBar
+export default NavBar;
