89dcd401ca0fa8079feb6de6a8329ddac9048f29
retrospecht <brandonmspecht@gmail.com>
2024-11-04 14:37:28 -0500

    Linked message page to index

--- client/src/NavBar.jsx
+++ client/src/NavBar.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 
 function NavBar()
 {
@@ -5,7 +7,10 @@ function NavBar()
         <nav>
             <ul className="navBar">
                 <li>
-                    <a href="../messagepage.html">Message Page</a>
+                    <Link to="/">Home Page</Link>
+                </li>
+                <li>
+                    <Link to="/messagePage">Message Page</Link>
                 </li>
                 <li>
                     <a href="./signup.html">Sign-up Page</a>
--- client/src/indexComponent.jsx
+++ client/src/indexComponent.jsx
@@ -1,9 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
 import IndexPage from "./index";
+import MainMessagePage from './pages/mainmessagepage';
 import "./index.css"
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <IndexPage/>
+    },
+    {
+        path: "/messagePage",
+        element: <MainMessagePage/> 
+      },
+  ]);
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <IndexPage />
+        <RouterProvider router={router} />
     </StrictMode>,
 )
