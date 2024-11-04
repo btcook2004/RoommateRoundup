80264d6cae2fffb1aaae39aa4709c7b48dd1860b
retrospecht <brandonmspecht@gmail.com>
2024-11-04 15:53:17 -0500

    Ensuring all changes are committed

---
+++ client/src/pages/mainmessagepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from '../NavBar'
+import MessagePage from './messagepage'
+
+function MainMessagePage() {
+    return(
+    <div>
+        <NavBar></NavBar>
+        <MessagePage/>
+        <div>WOW IT WORKS!!!!!!</div>
+    </div>
+    )
+}
+
+export default MainMessagePage
