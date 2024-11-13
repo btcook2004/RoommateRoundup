cf420002c91ebe2780412920996c694027b88613
Jillian Bice <jcb22b@fsu.edu>
2024-11-13 14:55:25 -0500

    removed bs name

--- client/src/pages/dashboard.jsx
+++ client/src/pages/dashboard.jsx
@@ -5,7 +5,6 @@ function Dashboard()
       <div>
         
         <h1>Dashboard</h1>
-
         <h2>Placeholder for start swiping link</h2>
         <Link to="/swipePage">Swipe Page</Link>
         <h1>Placeholder for messages link</h1>
@@ -13,7 +12,6 @@ function Dashboard()
         <h1>placeholder for matching stats</h1>
         <h1>placeholder for edit profile</h1>
         <Link to="/editprofile">Edit Profile</Link>
-
 
       </div>
     );
