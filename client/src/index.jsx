d5304a8deb838b408bac60d663f0336f3c35f318
btcook2004 <btcook.2004@gmail.com>
2024-11-13 16:07:24 -0500

    BC + JB Modified formatting of homepage

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,35 @@
 // import NavBar from "./NavBar.jsx";
 import { Link } from 'react-router-dom'
+
+function linkToSignUp()
+{
+    window.location.href = "/signup";
+}
+
+function linkToSignIn()
+{
+    window.location.href = "/signin";
+}
+
 function IndexPage()
 {
     return (
       <div>
         {/* <NavBar /> */}
 
-          <Link to="/signup">Sign Up</Link>
+        <nav className="topBarContainer">
+            <img src={"/logo.svg"} alt={"a great logo"} className={"logo"}/>
+            <ul className="topBar">
+              <li>
+                <button type="button" onClick={linkToSignUp}>Sign Up</button>
+              </li>
+              <li>
+                <button type="button" onClick={linkToSignIn}>Sign In</button>
+              </li>
+            </ul>
+          </nav>
+
+          {/* <Link to="/signup">Sign Up</Link> */}
 
           <h1>Placeholder for sign-in link that will redirect to dashboard</h1>
           <h1>temp link to dashboard to bypass signin/signup:</h1>
