9ca4477b3c135a13a3b4fc5dac816a0f6e81c67a
libbyssauer <130522316+libbyssauer@users.noreply.github.com>
2024-11-14 15:04:47 -0500

    i made two very generic buttons

--- client/src/pages/swipe.jsx
+++ client/src/pages/swipe.jsx
@@ -4,6 +4,17 @@ function Swipe()
     <div>
       <div>
         <h1>Swipe</h1>
+        
+          <button id ="right" variant="contained"
+          onClick={() => {
+            alert('right');
+          }}> Swipe Right</button> 
+
+        <button id ="left" variant="contained" onClick={() => {
+        alert('left');
+        }}> 
+        Swipe Left</button>
+        
       </div>
     </div>
   );
